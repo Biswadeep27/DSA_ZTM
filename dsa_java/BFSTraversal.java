b50d3b4106a3549e511f15863763137d85fd2234
Biswadeep Upadhyay <biswadeep.upadhyay@lowes.com>
2022-07-29 09:55:22 +0530

    BFS

---
+++ dsa_java/BFSTraversal.java
@@ -0,0 +1,81 @@
+
+import java.util.*;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int x){
+        this.val = x;
+    }
+    TreeNode(int x, TreeNode left, TreeNode right) {
+        this.val = x;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BFSTraversal {
+    public static List<List<Integer>> levelOrder(TreeNode root ){
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        if (root != null){
+            q.offer(root);
+        }
+        TreeNode curr;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> subAns = new LinkedList<Integer>();
+            for (int i=0; i<size; ++i){
+                curr = q.poll();
+                subAns.add(curr.val);
+                if (curr.left != null){
+                    q.offer(curr.left);
+                }
+                if (curr.right != null){
+                    q.offer(curr.right);
+                }
+
+            }
+            ans.add(subAns);
+        }
+
+        return ans;
+    }
+
+    public static void main(String args[]){
+        TreeNode binary_tree = new TreeNode(3);
+        binary_tree.left = new TreeNode(2);
+        binary_tree.right = new TreeNode(4);
+        binary_tree.left.right = new TreeNode(1);
+        binary_tree.left.right.left = new TreeNode(6);
+        binary_tree.left.right.right = new TreeNode(7);
+        binary_tree.left.right.right.left = new TreeNode(8);
+        binary_tree.right.left = new TreeNode(3);
+        binary_tree.right.left.left = new TreeNode(4);
+        binary_tree.right.left.right = new TreeNode(5);
+
+        System.out.println("The root node is : " + (binary_tree.val));
+
+        /*int[][] matrix = new int[4][4];
+        for (int i = 0; i < matrix.length; i++) {
+            Arrays.fill(matrix[i],10);
+        }*/
+
+        List<List<Integer>> res ;
+
+        res = levelOrder(binary_tree);
+
+        for (int i=0; i<res.size(); i++){
+            for (int j=0; j<res.get(i).size(); j++){
+                System.out.print((res.get(i).get(j)) + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
--- dsa_java/FindNemo.java
+++ dsa_java/FindNemo.java
@@ -1,16 +1,32 @@
+import java.util.Arrays;
+import java.time.Duration;
+import java.time.Instant;
+
+
 public class FindNemo {
 
     public static void find_nemo(String[] array){
+        long st1 = System.nanoTime();
+        long st2 = System.currentTimeMillis();
+        Instant st3 = Instant.now();
         for (int i = 0; i < array.length; i++){
             if (array[i].equals("nemo")){
                 System.out.println("Nemo Found!");
             }
         }
+        long et1 = System.nanoTime();
+        long et2 = System.currentTimeMillis();
+        Instant et3 = Instant.now();
+        System.out.println("Time taken in seconds: " + (et1 - st1)/1000000000);
+        System.out.println("Time taken in milliseconds: " + (et2 - st2));
+        System.out.println("Time taken: " + Duration.between(st3,et3).toString());
     }
 
     public static void main(String args[]){
         String[] arr = new String[1];
         arr[0] = "nemo";
-        find_nemo(arr);
+        String[] large = new String[1000000];
+        Arrays.fill(large,"nemo");
+        find_nemo(large);
     }
 }
